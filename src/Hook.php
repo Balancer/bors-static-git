8cfa480bc02678fd41f3412cd6b19c2ae38946f7
Balancer <balancer@balancer.ru>
2016-11-04 01:09:13 +0300

    ++

--- src/Hook.php
+++ src/Hook.php
@@ -2,7 +2,16 @@
 
 namespace B2\WebRoot\Git;
 
-class Hook
+class Hook extends \B2\Page
 {
-
+	function pre_show()
+	{
+        \bors_debug::syslog('00-rpc', "Github");
+        \bors_debug::syslog('00-rpc', "payload=".print_r(json_decode(@$_POST['payload'], true), true));
+        // https://github.com/kzykhys/PHPGit
+        $git = new \PHPGit\Git();
+        $git->setRepository(\B2\Cfg::get('webroot.markdown.repo'));
+        $git->pull();
+        return "Pull ok";
+	}
 }
