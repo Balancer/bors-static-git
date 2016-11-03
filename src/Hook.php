b0a0f10776a5781e8fb8f3e6f9cda9183a73a7e1
Balancer <balancer@balancer.ru>
2016-11-04 01:54:11 +0300

    Worked

--- src/Hook.php
+++ src/Hook.php
@@ -6,12 +6,16 @@ class Hook extends \B2\Page
 {
 	function pre_show()
 	{
-        \bors_debug::syslog('00-rpc', "Github");
-        \bors_debug::syslog('00-rpc', "payload=".print_r(json_decode(@$_POST['payload'], true), true));
+		if(!\B2\Cfg::get('webroot.markdown.repo'))
+			throw new \Exception(_("Undefined config 'webroot.markdown.repo'"));
+
+//        \bors_debug::syslog('debug-rpc-post', "Github, POST=".print_r(@$_POST, true));
+//        \bors_debug::syslog('debug-rpc-payload', "payload=".print_r(json_decode(@$_POST['payload'], true), true));
         // https://github.com/kzykhys/PHPGit
         $git = new \PHPGit\Git();
         $git->setRepository(\B2\Cfg::get('webroot.markdown.repo'));
         $git->pull();
-        return "Pull ok";
+		echo "Pull ok";
+        return true;
 	}
 }
